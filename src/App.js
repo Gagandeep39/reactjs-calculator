2152383435939df6d893c81ea3e93dbc56406ac0
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-26 12:44:33 +0530

    Implemented Calculator Logic

--- src/App.js
+++ src/App.js
@@ -4,16 +4,54 @@ import KeypadComponent from './components/Keypad/KeypadComponent';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: '0',
+    };
+  }
 
   handleClick = (buttonType) => {
     console.log(buttonType);
+    if (buttonType === '=') this.calculateResult();
+    else if (buttonType === 'C') this.clearResult();
+    else if (buttonType === 'CE') this.backspace();
+    else {
+      this.setState({
+        result: this.state.result + buttonType,
+      });
+    }
+  };
+
+  calculateResult = () => {
+    try {
+      this.setState({
+        result: eval(this.state.result) + '',
+      });
+    } catch (e) {
+      this.setState({
+        result: 'error',
+      });
+    }
+  };
+
+  clearResult = () => {
+    this.setState({
+      result: '',
+    });
+  };
+
+  backspace = () => {
+    let equation = this.state.result;
+    equation.slice(0, 1);
+    this.setState({ result: equation });
   };
 
   render() {
     return (
       <div>
         <h1>Calculator</h1>
-        <ResultComponent />
+        <ResultComponent result={this.state.result} />
         <KeypadComponent buttonClick={this.handleClick} />
       </div>
     );
